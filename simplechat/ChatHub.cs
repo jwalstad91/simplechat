3af7963119e8aea23638351ef467175dba761944
Jason Walstad <jason.walstad@experian.com>
2017-08-10 14:09:58 -0400

    init

---
+++ simplechat/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.SignalR;
+using simplechat;
+
+namespace simplechat
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            Clients.All.broadcastMessage(name, message);
+        }
+    }
+}
---
+++ simplechat/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(simplechat.Startup))]
+
+namespace simplechat
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+
+            app.MapSignalR("/signalr", new Microsoft.AspNet.SignalR.HubConfiguration());
+        }
+    }
+}
